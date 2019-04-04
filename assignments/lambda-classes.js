3cc059b238ec3931a1bad127cf096d2c715bad97
HaBuDeSu <ruf.kenichi@gmail.com>
2019-04-03 20:41:35 -0500

    added Student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -28,3 +28,35 @@ class Instructor extends Person {
     console.log(`${student.name} receives a perfect score on ${subject}`)
   }
 }
+
+class Student extends Person {
+  constructor (attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground,
+    this.className = attributes.className,
+    this.favSubjects = attributes.favSubjects
+  }
+
+  listsSubjects () {
+    favoriteSubjects.forEach(subject => console.log(subject));
+  }
+
+  PRAssignment (subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`);
+  }
+
+  sprintChallenge () {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
+  }
+}
+
+// #### Project Manager
+//
+// * Now that we have instructors and students, we'd be nowhere without our PM's
+// * ProjectManagers are extensions of Instructors
+// * ProjectManagers have the following unique props:
+//   * `gradClassName`: i.e. CS1
+//   * `favInstructor`: i.e. Sean
+// * ProjectManagers have the following Methods:
+//   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+//   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
